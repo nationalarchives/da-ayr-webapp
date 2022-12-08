46f9a2cd0f45e52521d3961f08387742ed08c637
Luca Romagnoli <lromagnoli@zaizi.com>
2022-12-08 11:19:23 +0000

    get secrets from .env

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ The webapp code for the Access Your Records (AYR) project
 
 # Quickstart
 
+Generate a .env file in the root directory
+Add the following entry to the file
+    SECRET_KEY=your-secret-key
+
+You can generate a secret-key with a password manage or a free secret key generator services available online. Minimum length should be 50 chars. It should include alphanumeric values and symbols
+
     docker compose up --build
 
 This will build and run the Django app available at http://localhost:8000/
--- ayr_project/settings.py
+++ ayr_project/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
