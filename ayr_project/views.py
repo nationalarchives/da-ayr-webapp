fc8f32245bcba0631bb6b2ae96486f7e4bcdc27d
Luca Romagnoli <lromagnoli@zaizi.com>
2022-12-16 10:29:41 +0000

    add views.py

---
+++ ayr_project/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
