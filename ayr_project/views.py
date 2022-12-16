b20fb3b42d94af86328a89d50a2b1e4aed58814d
Luca Romagnoli <lromagnoli@zaizi.com>
2022-12-16 15:50:48 +0000

    black

--- ayr_project/views.py
+++ ayr_project/views.py
@@ -4,5 +4,5 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template("index.html")
     return HttpResponse(template.render({}, request))
