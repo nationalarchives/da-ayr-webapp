de4ac840776d94b77eb718e25c79dfd9b3b13b6d
Luca Romagnoli <lromagnoli@zaizi.com>
2022-12-19 16:31:36 +0000

    add postgres db for django app

--- ayr_project/settings.py
+++ ayr_project/settings.py
@@ -82,10 +82,16 @@ WSGI_APPLICATION = "ayr_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["WEBAPP_DB_NAME"],
+        'USER': os.environ["WEBAPP_DB_USER"],
+        'PASSWORD': os.environ["WEBAPP_DB_PASSWORD"],
+        'HOST': 'webapp-db',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
