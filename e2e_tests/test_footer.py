74fdac4ad2e6ae7777e4cc989755b4a42f6e974f
Candace Lebby <clebby@zaizi.com>
2023-11-09 12:54:46 +0000

    Merge branch 'main' of https://github.com/nationalarchives/da-ayr-beta-webapp into AYR-442/sign-out-screen-1

---
+++ e2e_tests/test_footer.py
@@ -0,0 +1,52 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+# Define list of footer links and their expected URLS
+
+footer_links = [
+    (
+        "Terms of use",
+        "/terms-of-use",
+        "Terms of use – AYR - Access Your Records – GOV.UK ",
+    ),
+    (
+        "Privacy",
+        "/privacy",
+        "Privacy notice – AYR - Access Your Records – GOV.UK ",
+    ),
+    (
+        "Cookies policy",
+        "/cookies",
+        "Cookies – AYR - Access Your Records – GOV.UK",
+    ),
+    (
+        "Accessibility",
+        "/accessibility",
+        "Accessibility statement – AYR - Access Your Records – GOV.UK",
+    ),
+]
+
+
+@pytest.fixture
+def setup_page(page: Page):
+    page.goto("/")
+    yield page
+
+
+@pytest.mark.parametrize(
+    "link_text, expected_url, expected_title", footer_links
+)
+def test_footer_links(setup_page, link_text, expected_url, expected_title):
+    # locate and click footer link as user would
+
+    setup_page.click(f'text="{link_text}"')
+
+    # Wait for navigation and check URL is correct
+
+    setup_page.wait_for_url(expected_url, timeout=5000)
+
+    # Assertions
+
+    # Check if the text of the link is contained within title of the page
+
+    expect(setup_page).to_have_title(expected_title)
