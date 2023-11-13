35a895675ce101f4fdc7aa413a76a6056159b1af
Candace Lebby <clebby@zaizi.com>
2023-11-13 12:08:37 +0000

    Pre-commit

--- app/tests/test_start-page.py
+++ app/tests/test_start-page.py
@@ -1,8 +1,12 @@
-
 def test_start_page(client):
     response = client.get("/start-page")
 
     assert response.status_code == 200
     assert b'<h2 class="govuk-heading-m">Before you start</h2>' in response.data
-    assert b'<h1 class="govuk-heading-l">Access your records</h1>' in response.data
-    assert b'''<a href="#" role="button" draggable="false" class="govuk-button govuk-button--start" data-module="govuk-button">Start now''' in response.data
+    assert (
+        b'<h1 class="govuk-heading-l">Access your records</h1>' in response.data
+    )
+    assert (
+        b"""<a href="#" role="button" draggable="false" class="govuk-button govuk-button--start" data-module="govuk-button">Start now"""
+        in response.data
+    )
