fa3c5f99069d033dcb1d7deb99dd8d7e4b658996
Candace Lebby <clebby@zaizi.com>
2023-11-15 11:05:17 +0000

    simple signed out page tests written

---
+++ app/tests/test_signed-out-page.py
@@ -0,0 +1,16 @@
+def test_signed_out_page(client):
+    response = client.get("/signed-out")
+
+    assert response.status_code == 200
+    assert (
+        b'<h1 class="govuk-heading-l">You have successfully signed out</h1>'
+        in response.data
+    )
+    assert (
+        b'<p class="govuk-body-l">Thank you for using Access Your Records.</p>'
+        in response.data
+    )
+    assert (
+        b'<a href="#" role="button" class="govuk-button govuk-button--sign-in-again" '
+        b'data-module="govuk-button">Sign back in'
+    ) in response.data
