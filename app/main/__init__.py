ec7e5291771127b4ce642856ace2cd3e15ed2d16
Anthony Hashemi <ant.hashemi@gmail.com>
2023-11-16 15:18:30 +0000

    Only show sign out button on views that require authentication

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,5 @@ from flask import Blueprint
 
 bp = Blueprint("main", __name__, template_folder="../templates/main")
 
+from app.main import context_processors  # noqa: E402,F401
 from app.main import routes  # noqa: E402,F401
--- app/main/authorize/keycloak_login_required_decorator.py
+++ app/main/authorize/keycloak_login_required_decorator.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
 import keycloak
-from flask import current_app, flash, redirect, session, url_for
+from flask import current_app, flash, g, redirect, session, url_for
 
 
 def access_token_login_required(view_func):
@@ -36,41 +36,49 @@ def access_token_login_required(view_func):
 
     @wraps(view_func)
     def decorated_view(*args, **kwargs):
-        if current_app.config["TESTING"] and not current_app.config.get(
-            "FORCE_AUTHENTICATION_FOR_IN_TESTING"
-        ):
-            return view_func(*args, **kwargs)
+        g.access_token_login_required = True  # Set attribute on g
+        try:
+            if current_app.config["TESTING"] and not current_app.config.get(
+                "FORCE_AUTHENTICATION_FOR_IN_TESTING"
+            ):
+                return view_func(*args, **kwargs)
 
-        access_token = session.get("access_token")
-        if not access_token:
-            return redirect(url_for("main.login"))
+            access_token = session.get("access_token")
+            if not access_token:
+                return redirect(url_for("main.login"))
 
-        keycloak_openid = keycloak.KeycloakOpenID(
-            server_url=current_app.config["KEYCLOAK_BASE_URI"],
-            client_id=current_app.config["KEYCLOAK_CLIENT_ID"],
-            realm_name=current_app.config["KEYCLOAK_REALM_NAME"],
-            client_secret_key=current_app.config["KEYCLOAK_CLIENT_SECRET"],
-        )
-
-        decoded_token = keycloak_openid.introspect(access_token)
-
-        if not decoded_token["active"]:
-            session.clear()
-            return redirect(url_for("main.login"))
-
-        keycloak_ayr_user_group = current_app.config["KEYCLOAK_AYR_USER_GROUP"]
-        if not _check_if_user_has_access_to_ayr(
-            keycloak_ayr_user_group, decoded_token
-        ):
-            flash(
-                "TNA User is logged in but does not have access to AYR. Please contact your admin."
+            keycloak_openid = keycloak.KeycloakOpenID(
+                server_url=current_app.config["KEYCLOAK_BASE_URI"],
+                client_id=current_app.config["KEYCLOAK_CLIENT_ID"],
+                realm_name=current_app.config["KEYCLOAK_REALM_NAME"],
+                client_secret_key=current_app.config["KEYCLOAK_CLIENT_SECRET"],
             )
-            return redirect(url_for("main.index"))
 
-        return view_func(*args, **kwargs)
-    
+            decoded_token = keycloak_openid.introspect(access_token)
+
+            if not decoded_token["active"]:
+                session.clear()
+                return redirect(url_for("main.login"))
+
+            keycloak_ayr_user_group = current_app.config[
+                "KEYCLOAK_AYR_USER_GROUP"
+            ]
+            if not _check_if_user_has_access_to_ayr(
+                keycloak_ayr_user_group, decoded_token
+            ):
+                flash(
+                    "TNA User is logged in but does not have access to AYR. Please contact your admin."
+                )
+                return redirect(url_for("main.index"))
+
+            return view_func(*args, **kwargs)
+        finally:
+            g.access_token_login_required = (
+                False  # Clear attribute after view execution
+            )
+
     decorated_view.access_token_login_required = True
-    
+
     return decorated_view
 
 
