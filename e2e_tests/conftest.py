7a68da038772b19758a3fa56e6571cf03102c219
Colin B <colinbowen2@gmail.com>
2023-12-08 09:14:46 +0000

    add test_css decorator and prefix check to run tests with css enabled

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -2,6 +2,8 @@ import os
 
 import pytest
 
+from e2e_tests.utils import block_css_decorator
+
 
 @pytest.fixture()
 def authenticated_page(page):
@@ -11,3 +13,9 @@ def authenticated_page(page):
     page.get_by_role("button", name="Sign in").click()
     page.wait_for_url("/poc-search-view")
     return page
+
+
+@pytest.fixture(autouse=True)
+def apply_block_css_decorator(request, page):
+    if "test_css_" not in request.node.name and callable(request.node.obj):
+        request.node.obj = block_css_decorator(request.node.obj)(page)
--- e2e_tests/test_homepage.py
+++ e2e_tests/test_homepage.py
@@ -2,11 +2,15 @@ import re
 
 from playwright.sync_api import Page, expect
 
-from e2e_tests.utils import block_css_decorator
 
-
-@block_css_decorator
 def test_has_title(page: Page):
+    page.goto("/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("AYR - Access Your Records – GOV.UK"))
+
+
+def test_css_has_title(page: Page):
     page.goto("/")
 
     # Expect a title "to contain" a substring.
