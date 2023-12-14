9018445e78c17ab2735c916ef85bd9a9a5816267
Colin B <colinbowen2@gmail.com>
2023-12-14 15:49:09 +0000

    Add tests

--- app/tests/test_browse.py
+++ app/tests/test_browse.py
@@ -32,3 +32,57 @@ def test_submit_search_query(client: FlaskClient):
     assert b"Records found 2" in response.data
     assert b"test series1" in response.data
     assert b"test series2" in response.data
+
+
+def test_browse_series(client: FlaskClient):
+    """
+    Given a user accessing the search page
+    When they make a GET request with a series id
+    Then they should see the series page content.
+    """
+    files = create_two_test_files()
+    file = files[0]
+    series_id = file.file_consignments.SeriesId
+
+    response = client.get(f"/browse?series={series_id}")
+
+    assert response.status_code == 200
+    assert b"Search for digital records" in response.data
+    assert b"Records found 1" in response.data
+    assert b"test body1" in response.data
+
+
+def test_browse_consignment(client: FlaskClient):
+    """
+    Given a user accessing the search page
+    When they make a GET request with a consignment id
+    Then they should see the consignment page content.
+    """
+    files = create_two_test_files()
+    file = files[0]
+    consignment_id = file.file_consignments.ConsignmentId
+
+    response = client.get(f"/browse?consignment_id={consignment_id}")
+
+    assert response.status_code == 200
+    assert b"Search for digital records" in response.data
+    assert b"Records found 2" in response.data
+
+
+def test_browse_transferring_body(client: FlaskClient):
+    """
+    Given a user accessing the search page
+    When they make a GET request with a transferring body id
+    Then they should see the transferring body page content.
+    """
+
+    files = create_two_test_files()
+    file = files[0]
+    transferring_body_id = file.file_consignments.consignment_bodies.BodyId
+
+    response = client.get(f"/browse?transferring_body={transferring_body_id}")
+
+    assert response.status_code == 200
+    assert b"Search for digital records" in response.data
+    assert b"Records found 1" in response.data
+    assert b"test body1" in response.data
