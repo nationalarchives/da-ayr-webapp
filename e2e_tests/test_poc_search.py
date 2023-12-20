0f40768ce62b55ca19a4accd70976ef7581a4719
Pavan Gandhi <pgandhi@zaizi.com>
2023-12-20 09:47:02 +0000

    fixed e2e_test issues

--- e2e_tests/test_poc_search.py
+++ e2e_tests/test_poc_search.py
@@ -56,3 +56,174 @@ def test_poc_search_end_to_end(authenticated_page: Page):
         "file-a2.txt",
     ]
     assert expected_entries == cell_texts
+
+
+def test_pagination_available(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.fill("#searchInput", "Testing A")
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+    assert (
+        authenticated_page.locator(
+            ".govuk-pagination__link-title"
+        ).first.inner_text()
+        == "Next page"
+    )
+
+
+def test_pagination_check_only_one_page_returned(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.is_visible("text='Pagination'")
+    authenticated_page.fill("#searchInput", "Test description")
+    expect(authenticated_page.locator("#searchInput")).to_have_value(
+        "Test description"
+    )
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+    assert (
+        authenticated_page.locator(".govuk-pagination__prev").first.inner_text()
+        == ""
+    )
+    assert (
+        authenticated_page.locator(".govuk-pagination__next").first.inner_text()
+        == ""
+    )
+
+
+def test_pagination_get_first_page(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.fill("#searchInput", "Testing A")
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+    url = "/poc-search?page=1&query=testing+a"
+    assert (
+        authenticated_page.locator(
+            ".govuk-pagination__link"
+        ).first.get_attribute("href")
+        == url
+    )
+    links = authenticated_page.locator(".govuk-pagination__link-title").all()
+    assert links[0].inner_text() == "Next page"
+    expect(authenticated_page.get_by_text("5 record(s) found"))
+    rows = authenticated_page.locator(".govuk-table__row").all()
+    assert len(rows) == 6  # including header row
+
+
+def test_pagination_get_previous_page(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.fill("#searchInput", "Testing A")
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    page_links = authenticated_page.locator(".govuk-pagination__link").all()
+    if len(page_links) > 1:
+        authenticated_page.get_by_role("link").get_by_text(
+            page_links[1].inner_text()
+        ).click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+    links = authenticated_page.locator(".govuk-pagination__link-title").all()
+    assert links[0].inner_text() == "Previous page"
+
+
+def test_pagination_click_previous_link(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.fill("#searchInput", "Testing A")
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+
+    authenticated_page.get_by_role("link").get_by_text(" 2 ").click()
+
+    links = authenticated_page.locator(".govuk-pagination__link-title").all()
+    assert links[0].inner_text() == "Previous page"
+
+    print(authenticated_page.url)
+    url = "/poc-search?page=1&query=testing+a"
+    authenticated_page.expect_response(url)
+    # expect(authenticated_page.expect_response(url))
+
+
+def test_pagination_get_next_page(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.fill("#searchInput", "Testing A")
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+    url = "/poc-search?page=1&query=testing+a"
+    assert (
+        authenticated_page.locator(
+            ".govuk-pagination__link"
+        ).first.get_attribute("href")
+        == url
+    )
+    authenticated_page.get_by_role("link").get_by_text(" 2 ").click()
+    links = authenticated_page.locator(".govuk-pagination__link-title").all()
+    assert links[0].inner_text() == "Previous page"
+    if len(links) > 1:
+        assert links[1].inner_text() == "Next page"
+
+
+def test_pagination_get_ellipses_page(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.fill("#searchInput", "Testing A")
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+    page_links = authenticated_page.locator(".govuk-pagination__link").all()
+    last_page = page_links[len(page_links) - 1].inner_text()
+    assert page_links[0].inner_text() == "1"
+    assert page_links[1].inner_text() == "2"
+    assert page_links[3].inner_text() == last_page
+    ellipsis_link = authenticated_page.locator(
+        ".govuk-pagination__item--ellipses"
+    ).all()
+    assert ellipsis_link[0].inner_text() == "⋯"
+    links = authenticated_page.locator(".govuk-pagination__link-title").all()
+    assert links[0].inner_text() == "Next page"
+
+
+def test_pagination_get_last_page(authenticated_page: Page):
+    authenticated_page.goto("/poc-search")
+    authenticated_page.fill("#searchInput", "Testing A")
+    authenticated_page.get_by_role("button").get_by_text("Search").click()
+    assert (
+        authenticated_page.locator(".govuk-pagination").first.get_attribute(
+            "aria-label"
+        )
+        == "Pagination"
+    )
+    page_links = authenticated_page.locator(".govuk-pagination__link").all()
+    last_page = page_links[len(page_links) - 1].inner_text()
+    authenticated_page.get_by_role("link").get_by_text(last_page).click()
+    links = authenticated_page.locator(".govuk-pagination__link-title").all()
+    assert links[0].inner_text() == "Previous page"
