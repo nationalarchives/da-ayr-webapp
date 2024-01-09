55cc4f713051d7a540cd7754118a596db7491a10
Rahat Ahmed <rahmed@zaizi.com>
2024-01-09 09:33:37 +0000

    unit tests added for download route

--- app/tests/test_download.py
+++ app/tests/test_download.py
@@ -1,12 +1,16 @@
 import boto3
 from moto import mock_s3
 
-from app.tests.conftest import mock_standard_user
+from app.tests.conftest import mock_standard_user, mock_superuser
 from app.tests.factories import (
     ConsignmentFactory,
     FileFactory,
     FileMetadataFactory,
 )
+
+BUCKET = "test-download"
+CONSIGNMENT_REF = "TDR-123-TEST"
+FILE_PATH = "data/content/folder_a/test_file.txt"
 
 
 def test_invalid_id_raises_404(client):
@@ -21,37 +25,14 @@ def test_invalid_id_raises_404(client):
     assert response.status_code == 404
 
 
-@mock_s3
-def test_returns_record_page_for_user_with_access_to_files_transferring_body(
-    app, client
-):
-    """
-    Given a File in the database
-    When a standard user with access to the file's transferring body makes a
-        request to view the record page
-    Then the response status code should be 200
-    And the HTML content should contain specific elements
-        related to the record
-    """
-    consignment = ConsignmentFactory(ConsignmentReference="TDR-123-TEST")
+def mock_record():
+    consignment = ConsignmentFactory(ConsignmentReference=CONSIGNMENT_REF)
     file = FileFactory(
         file_consignments=consignment,
         FileName="test_file.txt",
-        FilePath="data/content/folder_a/test_file.txt",
+        FilePath=FILE_PATH,
         FileType="file",
     )
-
-    app.config["RECORD_BUCKET_NAME"] = "foo"
-    s3 = boto3.resource("s3", region_name="us-east-1")
-
-    s3.create_bucket(Bucket="foo")
-
-    object = s3.Object(
-        "foo", "TDR-123-TEST/data/content/folder_a/test_file.txt"
-    )
-    object.put(Body="foobar")
-
-    mock_standard_user(client, file.file_consignments.consignment_bodies.Name)
 
     metadata = {
         "date_last_modified": "2023-02-25T10:12:47",
@@ -72,6 +53,72 @@ def test_returns_record_page_for_user_with_access_to_files_transferring_body(
         for property_name, value in metadata.items()
     ]
 
+    return file
+
+
+@mock_s3
+def create_mock_s3_bucket_with_object():
+    s3 = boto3.resource("s3", region_name="us-east-1")
+
+    bucket = s3.create_bucket(Bucket=BUCKET)
+
+    object = s3.Object(BUCKET, f"{CONSIGNMENT_REF}/{FILE_PATH}")
+    object.put(Body="foobar")
+    return bucket
+
+
+@mock_s3
+def test_downloads_record_successfully_for_user_with_access_to_files_transferring_body(
+    app, client
+):
+    """
+    Given a File in the database
+    When a standard user with access to the file's transferring body makes a
+        request to view the record page
+    Then the response status code should be 200
+    And the HTML content should contain specific elements
+        related to the record
+    """
+    create_mock_s3_bucket_with_object()
+    app.config["RECORD_BUCKET_NAME"] = BUCKET
+    file = mock_record()
+    mock_standard_user(client, file.file_consignments.consignment_bodies.Name)
+    response = client.get(f"/download/{file.FileId}")
+
+    assert response.status_code == 200
+
+
+@mock_s3
+def test_raises_404_for_user_without_access_to_files_transferring_body(
+    app, client
+):
+    """
+    Given a File in the database
+    When a standard user without access to the file's consignment body makes a
+        request to download record
+    Then the response status code should be 404
+    """
+    create_mock_s3_bucket_with_object()
+    app.config["RECORD_BUCKET_NAME"] = BUCKET
+    file = mock_record()
+    mock_standard_user(client, "different_body")
+    response = client.get(f"/download/{file.FileId}")
+
+    assert response.status_code == 404
+
+
+@mock_s3
+def test_downloads_record_successfully_for_superuser(app, client):
+    """
+    Given a File in the database
+    And a superuser
+    When the superuser makes a request to download the record
+    Then the response status code should be 200
+    """
+    create_mock_s3_bucket_with_object()
+    app.config["RECORD_BUCKET_NAME"] = BUCKET
+    file = mock_record()
+    mock_superuser(client)
     response = client.get(f"/download/{file.FileId}")
 
     assert response.status_code == 200
