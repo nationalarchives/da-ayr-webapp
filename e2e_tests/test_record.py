3012cefb9ae9f7e812655587f1e3d75af8608f17
Rahat Ahmed <rahmed@zaizi.com>
2024-01-09 17:04:03 +0000

    file and function name changed

--- e2e_tests/test_record.py
+++ e2e_tests/test_record.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import Page
 
 
-def test_can_download_file_by_clicking_button(authenticated_page: Page):
+def test_download_record(authenticated_page: Page):
     authenticated_page.goto("/record/d806615f-c37e-48e5-a2f5-2281846cf1a3")
     with authenticated_page.expect_download() as download_record:
         authenticated_page.get_by_text("Download record").click()
