df29f785795a2c9404cdb5ec9564b4780c430435
Pavan Gandhi <pgandhi@zaizi.com>
2024-01-19 16:42:17 +0000

    re check in to see if failed unit test  test_build_fuzzy_search_query_no_results passes successfully

--- app/main/routes.py
+++ app/main/routes.py
@@ -137,6 +137,7 @@ def browse():
         # sorting_orders["last_record_transferred"] = "desc"  # most recent first
         # sorting_orders["records_held"] = "asc"  # least first
         # sorting_orders["records_held"] = "desc"  # most first
+
     elif consignment_id:
         browse_type = "consignment"
         browse_parameters["consignment_id"] = consignment_id
