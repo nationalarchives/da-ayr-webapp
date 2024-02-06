e6fbedb6b2c5f3e66a311f2749c63e9f4b78966d
Colin B <colinbowen2@gmail.com>
2024-02-06 22:25:56 +0000

    fix: pre commit

--- app/main/routes.py
+++ app/main/routes.py
@@ -36,7 +36,7 @@ from app.main.forms import CookiesForm
 from app.main.util.filter_sort_builder import (
     build_filters,
     build_sorting_orders,
-    build_sorting_orders_consignment
+    build_sorting_orders_consignment,
 )
 
 from .forms import SearchForm
