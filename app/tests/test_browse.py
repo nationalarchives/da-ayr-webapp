7cc95c7ab152dbdb6be12db6557a5d37be00da52
Colin B <colinbowen2@gmail.com>
2024-02-07 17:54:24 +0000

    Pre commit, coverage

--- app/tests/test_browse.py
+++ app/tests/test_browse.py
@@ -113,6 +113,13 @@ def verify_data_rows(data, expected_rows):
         if row_index < len(rows) - 1:
             row_data = row_data + ", "
 
+    print("actual")
+    print(row_data)
+
+    print("expected")
+    print(expected_rows)
+    print("")
+
     assert [row_data] == expected_rows[0]
 
 
@@ -3048,6 +3055,90 @@ class TestConsignment:
             "&date_from_day=01"
             "&date_from_month=01"
             "&date_from_year=2020"
+            "&date_to_day=10"
+            "&date_to_month=03"
+            "&date_to_year=2023"
+        )
+
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert b"You are viewing" in response.data
+
+        expected_rows = [
+            [
+                "'15/01/2023', 'second_file.ppt', 'Open', '-', '-', "
+                "'25/02/2023', 'first_file.docx', 'Closed', '25/02/2023', '10 years', "
+                "'10/03/2023', 'third_file.docx', 'Closed', '10/03/2023', '25 years'"
+            ],
+        ]
+
+        verify_consignment_view_header_row(response.data)
+        verify_data_rows(response.data, expected_rows)
+
+    def test_browse_consignment_sort_and_date_from_filter(
+        self, client: FlaskClient, mock_standard_user, browse_consignment_files
+    ):
+        """
+        Given a user accessing the browse page
+        When they make a GET request with a consignment id
+        Then they should be able to sort the results by the available options and be
+        able to filter the available results by date
+        """
+        consignment_id = browse_consignment_files[0].consignment.ConsignmentId
+
+        mock_standard_user(
+            client, browse_consignment_files[0].consignment.series.body.Name
+        )
+
+        url = (
+            f"/browse?consignment_id={consignment_id}"
+            "&sort=closure_type-desc"
+            "&record_status=all"
+            "&date_filter_field=date_last_modified"
+            "&date_from_day=01"
+            "&date_from_month=01"
+            "&date_from_year=2020"
+        )
+
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert b"You are viewing" in response.data
+
+        expected_rows = [
+            [
+                "'20/05/2023', 'fifth_file.doc', 'Open', '-', '-', "
+                "'15/01/2023', 'second_file.ppt', 'Open', '-', '-', "
+                "'25/02/2023', 'first_file.docx', 'Closed', '25/02/2023', '10 years', "
+                "'12/04/2023', 'fourth_file.xls', 'Closed', '12/04/2023', '70 years', "
+                "'10/03/2023', 'third_file.docx', 'Closed', '10/03/2023', '25 years'"
+            ],
+        ]
+
+        verify_consignment_view_header_row(response.data)
+        verify_data_rows(response.data, expected_rows)
+
+    def test_browse_consignment_sort_and_date_to_filter(
+        self, client: FlaskClient, mock_standard_user, browse_consignment_files
+    ):
+        """
+        Given a user accessing the browse page
+        When they make a GET request with a consignment id
+        Then they should be able to sort the results by the available options and be
+        able to filter the available results by date
+        """
+        consignment_id = browse_consignment_files[0].consignment.ConsignmentId
+
+        mock_standard_user(
+            client, browse_consignment_files[0].consignment.series.body.Name
+        )
+
+        url = (
+            f"/browse?consignment_id={consignment_id}"
+            "&sort=closure_type-desc"
+            "&record_status=all"
+            "&date_filter_field=date_last_modified"
             "&date_to_day=10"
             "&date_to_month=03"
             "&date_to_year=2023"
