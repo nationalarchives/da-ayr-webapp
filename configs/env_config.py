cb90b7ba6f739f9db5277942654281e09fecfc5c
Pavan Gandhi <pgandhi@zaizi.com>
2024-04-15 17:38:10 +0100

    updated env_config.py to check if variable value set in .env file, if not raise an exception when app starts

--- configs/env_config.py
+++ configs/env_config.py
@@ -5,4 +5,17 @@ from configs.base_config import BaseConfig
 
 class EnvConfig(BaseConfig):
     def _get_config_value(self, variable_name):
-        return os.getenv(variable_name)
+        if variable_name in (
+            "AWS_REGION",
+            "FLASKS3_ACTIVE",
+            "FLASKS3_CDN_DOMAIN",
+            "FLASKS3_BUCKET_NAME",
+        ):
+            return os.getenv(variable_name)
+
+        if os.getenv(variable_name) is not None:
+            return os.getenv(variable_name)
+        else:
+            raise Exception(
+                f"variable name : '{variable_name}' has not been set in .env file"
+            )
