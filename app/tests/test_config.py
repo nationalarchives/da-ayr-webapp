b16f122cdcdaef91c727fb4b5e36d88284bb3ac7
Pavan Gandhi <pgandhi@zaizi.com>
2024-04-16 11:59:42 +0100

    added logic in env_config.py and aws_secrets_manager_config.py to validate whether variable values are set, added test cases in test_config.py to verify that exception is thrown when variable value not set

--- app/tests/test_config.py
+++ app/tests/test_config.py
@@ -1,6 +1,8 @@
+import inspect
 import json
 
 import boto3
+import pytest
 from moto import mock_aws
 
 from configs.aws_secrets_manager_config import AWSSecretsManagerConfig
@@ -57,6 +59,47 @@ def test_local_env_vars_config_initialized(monkeypatch):
     assert config.FLASKS3_ACTIVE is False
     assert config.FLASKS3_CDN_DOMAIN == "test_flasks3_cdn_domain"
     assert config.FLASKS3_BUCKET_NAME == "test_flasks3_bucket_name"
+
+
+def test_local_env_config_variable_not_set_error(monkeypatch):
+    """
+    GIVEN an environment variable 'DEFAULT_DATE_FORMAT' is not set in local .env file
+    WHEN Config is initialized
+    THEN it should raise an exception with error
+    """
+    monkeypatch.setenv(
+        "SQLALCHEMY_DATABASE_URI", "test_sqlalchemy_database_uri"
+    )
+    monkeypatch.setenv("DB_PORT", "5432")
+    monkeypatch.setenv("DB_HOST", "test_db_host")
+    monkeypatch.setenv("DB_USER", "test_db_user")
+    monkeypatch.setenv("DB_PASSWORD", "test_db_password")
+    monkeypatch.setenv("DB_NAME", "test_db_name")
+    monkeypatch.setenv("KEYCLOAK_BASE_URI", "test_keycloak_base_uri")
+    monkeypatch.setenv("KEYCLOAK_CLIENT_ID", "test_keycloak_client_id")
+    monkeypatch.setenv("KEYCLOAK_REALM_NAME", "test_keycloak_realm_name")
+    monkeypatch.setenv(
+        "KEYCLOAK_CLIENT_SECRET", "test_keycloak_client_secret"
+    )  # pragma: allowlist secret
+    monkeypatch.setenv(
+        "SECRET_KEY", "test_secret_key"  # pragma: allowlist secret
+    )
+    monkeypatch.setenv("DEFAULT_PAGE_SIZE", "test_default_page_size")
+    monkeypatch.setenv("RECORD_BUCKET_NAME", "test_record_bucket_name")
+    monkeypatch.setenv("AWS_REGION", "test_region")
+    monkeypatch.setenv("FLASKS3_ACTIVE", "False")
+    monkeypatch.setenv("FLASKS3_CDN_DOMAIN", "test_flasks3_cdn_domain")
+    monkeypatch.setenv("FLASKS3_BUCKET_NAME", "test_flasks3_bucket_name")
+
+    config = EnvConfig()
+
+    with pytest.raises(Exception) as error:
+        inspect.getmembers(config)
+
+    assert (
+        str(error.value)
+        == "variable name : 'DEFAULT_DATE_FORMAT' has not been set in .env file"
+    )
 
 
 @mock_aws
@@ -123,3 +166,52 @@ def test_aws_secrets_manager_config_initialized(monkeypatch):
     assert config.FLASKS3_ACTIVE is False
     assert config.FLASKS3_CDN_DOMAIN == "test_flasks3_cdn_domain"
     assert config.FLASKS3_BUCKET_NAME == "test_flasks3_bucket_name"
+
+
+@mock_aws
+def test_aws_secrets_manager_config_variable_not_set_error(monkeypatch):
+    """
+    GIVEN AWS secret with secret_id `AWS_SM_CONFIG_SECRET_ID` is set
+    and a variable 'DEFAULT_DATE_FORMAT' is not set
+    WHEN Config is initialized
+    THEN it should raise an exception with error
+    """
+    secret_value = json.dumps(
+        {
+            "AWS_REGION": "test_aws_region",
+            "KEYCLOAK_BASE_URI": "test_keycloak_base_uri",
+            "KEYCLOAK_CLIENT_ID": "test_keycloak_client_id",
+            "KEYCLOAK_REALM_NAME": "test_keycloack_realm_name",
+            "KEYCLOAK_CLIENT_SECRET": "test_keycloak_client_secret",  # pragma: allowlist secret
+            "RECORD_BUCKET_NAME": "test_record_bucket_name",
+            "FLASKS3_ACTIVE": "False",
+            "FLASKS3_CDN_DOMAIN": "test_flasks3_cdn_domain",
+            "FLASKS3_BUCKET_NAME": "test_flasks3_bucket_name",
+            # "DEFAULT_DATE_FORMAT": "test_default_date_format",
+            "SECRET_KEY": "test_secret_key",  # pragma: allowlist secret
+            "DB_PORT": "5432",
+            "DB_HOST": "test_db_host",
+            "DB_USER": "test_db_user",
+            "DB_PASSWORD": "test_db_password",  # pragma: allowlist secret
+            "DB_NAME": "test_db_name",
+            "DEFAULT_PAGE_SIZE": "test_default_page_size",
+        }
+    )
+
+    ssm_client = boto3.client("secretsmanager")
+
+    ssm_client.create_secret(
+        Name="test_secret_id",
+        SecretString=secret_value,
+    )
+
+    monkeypatch.setenv(
+        "AWS_SM_CONFIG_SECRET_ID", "test_secret_id"
+    )  # pragma: allowlist secret
+
+    config = AWSSecretsManagerConfig()
+
+    with pytest.raises(KeyError) as error:
+        inspect.getmembers(config)
+
+    assert str(error.value) == "'DEFAULT_DATE_FORMAT'"
--- configs/env_config.py
+++ configs/env_config.py
@@ -5,14 +5,6 @@ from configs.base_config import BaseConfig
 
 class EnvConfig(BaseConfig):
     def _get_config_value(self, variable_name):
-        if variable_name in (
-            "AWS_REGION",
-            "FLASKS3_ACTIVE",
-            "FLASKS3_CDN_DOMAIN",
-            "FLASKS3_BUCKET_NAME",
-        ):
-            return os.getenv(variable_name)
-
         if os.getenv(variable_name) is not None:
             return os.getenv(variable_name)
         else:
