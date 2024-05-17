875bdf9f84566fc222462d155d84fe696727b6d1
LTrasca <ltrasca@zaizi.com>
2024-05-17 11:22:08 +0100

    comment fixes

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -20,12 +20,7 @@ def page(page, request) -> Page:
 
 
 @pytest.fixture
-def create_user_page(
-    page, browser_name
-) -> (
-    Page
-):  # FIXME: browser_name specified until https://github.com/microsoft/playwright-pytest/issues/172 fixed
-    # so that multiple browser flags in cli are honoured
+def create_user_page(page, browser_name) -> Page:
     def _create_user_page(username, password) -> Page:
         page.goto("/sign-in")
         page.get_by_label("Email address").fill(username)
