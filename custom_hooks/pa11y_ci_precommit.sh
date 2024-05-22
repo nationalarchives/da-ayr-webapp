b4c5cd7bdd238c1c78a43ee69a06d164f31e0985
LTrasca <ltrasca@zaizi.com>
2024-05-22 14:11:27 +0100

    npx

--- custom_hooks/pa11y_ci_precommit.sh
+++ custom_hooks/pa11y_ci_precommit.sh
@@ -2,10 +2,7 @@
 set +e
 
 export $(grep -v '^#' .env.e2e_tests | xargs)
-
-# Install pa11y-ci if not installed
-npm list | grep pa11y-ci || npm install --save-dev pa11y-ci --no-shrinkwrap
-OUTPUT=$(npm run precommit)
+OUTPUT=$(npx pa11y-ci --config configs/pa11y_ci_precommit.js)
 echo "${OUTPUT}"
 
 set -e
