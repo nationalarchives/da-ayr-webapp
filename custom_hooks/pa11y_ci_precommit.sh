8aab52708515cd2a94ab8242664ee6e84f97c996
LTrasca <ltrasca@zaizi.com>
2024-06-19 14:21:37 +0100

    moved some files around

--- custom_hooks/pa11y_ci_precommit.sh
+++ custom_hooks/pa11y_ci_precommit.sh
@@ -3,7 +3,7 @@ set +e
 
 if [ "$CI" = "true" ]; then exit 0; fi
 
-python e2e_tests/scripts/user_management.py
+python e2e_tests/utils_scripts/user_management.py
 # export $(grep -v '^#' .env | xargs)
 # OUTPUT=$(npx pa11y-ci --config configs/pa11y_ci_precommit.js)
 # echo "${OUTPUT}"
