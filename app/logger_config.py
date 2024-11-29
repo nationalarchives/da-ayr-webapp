c78bbb9e2a46c1a3606fba7f76b888cddfb497e2
Colin B <colinbowen2@gmail.com>
2024-11-29 16:51:36 +0000

    update logger config to json format

--- app/logger_config.py
+++ app/logger_config.py
@@ -1,17 +1,26 @@
 import logging
+import json
 
 from flask import has_request_context, request
 
 
 class RequestFormatter(logging.Formatter):
     def format(self, record):
+        log_record = {
+            "log_type": record.name,
+            "timestamp": self.formatTime(record),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "module": record.module,
+        }
+
         if has_request_context():
-            record.url = request.url
-            record.remote_addr = request.remote_addr
-        else:
-            record.url = None
-            record.remote_addr = None
-        return super().format(record)
+            log_record.update({
+                "remote_addr": request.remote_addr,
+                "url": request.url,
+            })
+
+        return json.dumps(log_record)
 
 
 def setup_logger(name, level, formatter):
@@ -26,18 +35,8 @@ def setup_logger(name, level, formatter):
 
 def setup_logging(app):
     """Set up loggers for the app."""
-    audit_log_formatter = RequestFormatter(
-        "AUDIT_LOG\n"
-        "[%(asctime)s] %(remote_addr)s requested %(url)s\n"
-        "%(levelname)s in %(module)s: %(message)s"
-    )
-    app.audit_logger = setup_logger(
-        "audit_logger", logging.INFO, audit_log_formatter
-    )
+    audit_log_formatter = RequestFormatter()
+    app.audit_logger = setup_logger("audit_logger", logging.INFO, audit_log_formatter)
 
-    app_log_formatter = RequestFormatter(
-        "APP_LOG\n"
-        "[%(asctime)s] %(remote_addr)s requested %(url)s\n"
-        "%(levelname)s in %(module)s: %(message)s"
-    )
+    app_log_formatter = RequestFormatter()
     app.app_logger = setup_logger("app_logger", logging.INFO, app_log_formatter)
