13303c168ebb3949bf9d9b368ffd5ab7a680f186
Colin B <colinbowen2@gmail.com>
2025-01-08 13:06:00 +0000

    update csp

--- app/__init__.py
+++ app/__init__.py
@@ -139,8 +139,6 @@ def create_app(config_class, database_uri=None):
             "'sha256-1u1O/sNzLBXqLGKzuRbVTI5abqBQBfKsNv3bH5iXOkg='",  # pragma: allowlist secret
             "'sha256-xDT4BUH+7vjNzOH1DSYRS8mdxJbvLVPYsb8hjk4Yccg='",  # pragma: allowlist secret
             "'sha256-ylK9YBCBEaApMPzc82Ol5H/Hd5kmcv3wQlT3Y5m7Kn4='",  # pragma: allowlist secret
-            "'unsafe-hashes'",
-            "'unsafe-inline'",
         ],
         "style-src-elem": [
             SELF,
