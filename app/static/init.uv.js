6d69b2cb3b1d193ce885ea225c63c579a2aae5fc
LTrasca <ltrasca@zaizi.com>
2025-03-04 16:49:55 +0000

    init

--- app/static/init.uv.js
+++ app/static/init.uv.js
@@ -73,11 +73,11 @@ document.addEventListener("DOMContentLoaded", function () {
   let uvElement = document.getElementById("uv");
   if (uvElement) {
     uvElement.style.width = "100%";
-    uvElement.style.height = "60vh";
+    uvElement.style.height = "70vh";
 
     // Apply media query for small devices
     if (window.matchMedia("(max-width: 810px)").matches) {
-      uvElement.style.height = "60vh";
+      uvElement.style.height = "70vh";
       uvElement.style.width = "85vw";
       uvElement.style.padding = "1rem";
     }
