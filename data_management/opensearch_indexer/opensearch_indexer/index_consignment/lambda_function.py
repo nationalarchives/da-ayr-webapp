5618822b4b95285804b220b6ddb669b06c3fc2b0
Rahat Ahmed <rahmed@zaizi.com>
2025-06-04 16:17:50 +0100

    reverting back changes for lambda_function.py

--- data_management/opensearch_indexer/opensearch_indexer/index_consignment/lambda_function.py
+++ data_management/opensearch_indexer/opensearch_indexer/index_consignment/lambda_function.py
@@ -10,7 +10,24 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event: Dict[str, Any], context: Any) -> None:
+    """
+    AWS Lambda handler function to trigger the indexing of consignment files into OpenSearch.
 
+    This function is invoked by an AWS event containing details of a consignment. It retrieves
+    the necessary parameters from the event and environment variables, then calls the indexing
+    function to process and index the files into OpenSearch.
+    Args:
+        event (Dict[str, Any]): The event data triggering the Lambda function. Expected to contain:
+            - `detail` (Dict[str, Any]): A dictionary with `parameters` that includes:
+                - `reference` (str): The consignment reference identifier.
+        context (Any): AWS Lambda context object (not used in this function).
+    Environment Variables:
+        BUCKET_NAME (str): The name of the S3 bucket where the files are stored.
+        SECRET_ID (str): The identifier for the AWS Secrets Manager secret containing database
+                         and OpenSearch credentials.
+    Raises:
+        Exception: If `consignment_reference` or `SECRET_ID` are missing.
+    """
     logger.info("Lambda started")
     logger.info("Event received: %s", json.dumps(event))
 
