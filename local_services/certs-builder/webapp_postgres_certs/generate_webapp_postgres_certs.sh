67fc8234527fdd5fbdcbd93732dfdfdf0972bf3f
Colin B <colinbowen2@gmail.com>
2025-06-06 16:51:52 +0100

    certs

--- local_services/certs-builder/webapp_postgres_certs/generate_webapp_postgres_certs.sh
+++ local_services/certs-builder/webapp_postgres_certs/generate_webapp_postgres_certs.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Create directory if it doesn't exist
+mkdir -p /certs/webapp_postgres_certs
+cd /certs/webapp_postgres_certs
+
 # Create a private key
 openssl genrsa -out root-ca.key 2048
 
@@ -11,3 +17,7 @@ openssl req -new -key postgres_localhost.key -out postgres_localhost.csr -subj "
 
 # (Optional) Create a CA certificate
 openssl x509 -req -in postgres_localhost.csr -CA root-ca.pem -CAkey root-ca.key -CAcreateserial -out postgres_localhost.crt -days 365 -sha256
+
+# Set proper permissions
+chmod 600 postgres_localhost.key root-ca.key
+chmod 644 postgres_localhost.crt root-ca.pem
