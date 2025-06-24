6eae9c67a5943586e9e3a9e244a7e7262627047a
LTrasca <ltrasca@zaizi.com>
2025-06-24 16:43:47 +0100

    updated comparison function and updated most screenshots

--- e2e_tests/utils/assertions.py
+++ e2e_tests/utils/assertions.py
@@ -7,32 +7,29 @@ from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 
 
-def compare_snapshot(baseline_path, screenshot_image):
+def compare_snapshot(baseline_path, actual_image):
     baseline_image = Image.open(baseline_path)
 
-    # ensure both images have the same dimensions
-    min_width = min(baseline_image.width, screenshot_image.width)
-    min_height = min(baseline_image.height, screenshot_image.height)
+    min_width = min(actual_image.width, baseline_image.width)
+    min_height = min(actual_image.height, baseline_image.height)
 
-    baseline_cropped = baseline_image.crop((0, 0, min_width, min_height))
-    screenshot_cropped = screenshot_image.crop((0, 0, min_width, min_height))
+    actual_cropped = actual_image.crop((0, 0, min_width, min_height))
+    expected_cropped = baseline_image.crop((0, 0, min_width, min_height))
 
-    baseline_np = np.array(baseline_cropped, dtype=np.uint8)
-    screenshot_np = np.array(screenshot_cropped, dtype=np.uint8)
+    actual_np = np.array(actual_cropped, dtype=np.uint8)
+    expected_np = np.array(expected_cropped, dtype=np.uint8)
 
-    if baseline_np.shape != screenshot_np.shape:
-        warnings.warn(f"Baseline shape: {baseline_np.shape}")
-        warnings.warn(f"Screenshot shape: {screenshot_np.shape}")
+    if actual_np.shape != expected_np.shape:
+        warnings.warn(f"Actual shape: {actual_np.shape}")
+        warnings.warn(f"Expected shape: {expected_np.shape}")
         raise ValueError("Image sizes do not match")
 
-    # dynamically adjust win_size based on image dimensions
-    min_side = min(baseline_np.shape[0], baseline_np.shape[1])
-    win_size = min(7, min_side)
+    win_size = min(min(actual_np.shape[0:2]), 7)
     if win_size % 2 == 0:
         win_size -= 1
 
     score, _diff = ssim(
-        baseline_np, screenshot_np, full=True, win_size=win_size
+        actual_np, expected_np, full=True, win_size=win_size, channel_axis=-1
     )
     return score >= 0.9, score
 
@@ -57,5 +54,3 @@ def assert_matches_snapshot(snapshot, device, page_name):
         )
         screenshot_image.save(baseline_path)
         return
-
-    print(f"No changes detected for {page_name} (score: {score}).")
