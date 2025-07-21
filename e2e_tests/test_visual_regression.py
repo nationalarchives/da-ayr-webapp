e1b088a8e604d27cf6196da0cc6a9ed2f1a5850d
Colin B <colinbowen2@gmail.com>
2025-07-21 17:42:28 +0100

    use png, fix ssl cert creation, visual reg tests setup, keycloak wait

--- e2e_tests/test_visual_regression.py
+++ e2e_tests/test_visual_regression.py
@@ -9,48 +9,48 @@ from .utils.assertions import assert_matches_snapshot
     [
         (
             "/how-to-use-this-service",
-            "how_to_use_this_service.jpg",
+            "how_to_use_this_service.png",
             {"width": 1280, "height": 4000},
         ),
-        ("/terms-of-use", "terms_of_use.jpg", {"width": 1280, "height": 4000}),
-        ("/privacy", "privacy.jpg", {"width": 1280, "height": 4000}),
-        ("/cookies", "cookies.jpg", {"width": 1280, "height": 4000}),
+        ("/terms-of-use", "terms_of_use.png", {"width": 1280, "height": 4000}),
+        ("/privacy", "privacy.png", {"width": 1280, "height": 4000}),
+        ("/cookies", "cookies.png", {"width": 1280, "height": 4000}),
         (
             "/accessibility",
-            "accessibility.jpg",
+            "accessibility.png",
             {"width": 1280, "height": 4000},
         ),
-        ("/signed-out", "signed_out.jpg", {"width": 1280, "height": 4000}),
-        ("/", "start.jpg", {"width": 1280, "height": 4000}),
-        ("/browse", "browse.jpg", {"width": 1280, "height": 4000}),
+        ("/signed-out", "signed_out.png", {"width": 1280, "height": 4000}),
+        ("/", "start.png", {"width": 1280, "height": 4000}),
+        ("/browse", "browse.png", {"width": 1280, "height": 4000}),
         (
             "/browse/transferring_body/8ccc8cd1-c0ee-431d-afad-70cf404ba337",
-            "browse_transferring_body.jpg",
+            "browse_transferring_body.png",
             {"width": 1280, "height": 4000},
         ),
         (
             "/browse/series/93ed0101-2318-45ab-8730-c681958ded7e",
-            "browse_series.jpg",
+            "browse_series.png",
             {"width": 1280, "height": 4000},
         ),
         (
             "/browse/consignment/2fd4e03e-5913-4c04-b4f2-5a823fafd430",
-            "browse_consignment.jpg",
+            "browse_consignment.png",
             {"width": 1280, "height": 4000},
         ),
         (
             "/search_results_summary?query=a",
-            "search_results_summary.jpg",
+            "search_results_summary.png",
             {"width": 1280, "height": 4000},
         ),
         (
             "/search/transferring_body/8ccc8cd1-c0ee-431d-afad-70cf404ba337?query=a&sort=series-asc&search_filter=test",
-            "search_transferring_body.jpg",
+            "search_transferring_body.png",
             {"width": 1280, "height": 4000},
         ),
         (
             "/record/123e4567-e89b-12d3-a456-426614174000",
-            "record.jpg",
+            "record.png",
             {"width": 1280, "height": 4000},
         ),
     ],
@@ -70,7 +70,7 @@ def test_css_no_visual_regression(
     aau_user_page.set_viewport_size(viewport)
     aau_user_page.goto(url, wait_until="networkidle")
     aau_user_page.wait_for_load_state("domcontentloaded")
-    snapshot = aau_user_page.screenshot(full_page=True, type="jpeg")
+    snapshot = aau_user_page.screenshot(full_page=True, type="png")
     assert_matches_snapshot(
         snapshot,
         device="desktop",
@@ -83,56 +83,56 @@ def test_css_no_visual_regression(
     [
         (
             "/how-to-use-this-service",
-            "how_to_use_this_service_mobile.jpg",
+            "how_to_use_this_service_mobile.png",
             {"width": 390, "height": 5000},
         ),
         (
             "/terms-of-use",
-            "terms_of_use_mobile.jpg",
+            "terms_of_use_mobile.png",
             {"width": 390, "height": 5000},
         ),
-        ("/privacy", "privacy_mobile.jpg", {"width": 390, "height": 5000}),
-        ("/cookies", "cookies_mobile.jpg", {"width": 390, "height": 5000}),
+        ("/privacy", "privacy_mobile.png", {"width": 390, "height": 5000}),
+        ("/cookies", "cookies_mobile.png", {"width": 390, "height": 5000}),
         (
             "/accessibility",
-            "accessibility_mobile.jpg",
+            "accessibility_mobile.png",
             {"width": 390, "height": 5000},
         ),
         (
             "/signed-out",
-            "signed_out_mobile.jpg",
+            "signed_out_mobile.png",
             {"width": 390, "height": 5000},
         ),
-        ("/", "start_mobile.jpg", {"width": 390, "height": 5000}),
-        ("/browse", "browse_mobile.jpg", {"width": 390, "height": 5000}),
+        ("/", "start_mobile.png", {"width": 390, "height": 5000}),
+        ("/browse", "browse_mobile.png", {"width": 390, "height": 5000}),
         (
             "/browse/transferring_body/8ccc8cd1-c0ee-431d-afad-70cf404ba337",
-            "browse_transferring_body_mobile.jpg",
+            "browse_transferring_body_mobile.png",
             {"width": 390, "height": 5000},
         ),
         (
             "/browse/series/93ed0101-2318-45ab-8730-c681958ded7e",
-            "browse_series_mobile.jpg",
+            "browse_series_mobile.png",
             {"width": 390, "height": 5000},
         ),
         (
             "/browse/consignment/2fd4e03e-5913-4c04-b4f2-5a823fafd430",
-            "browse_consignment_mobile.jpg",
+            "browse_consignment_mobile.png",
             {"width": 390, "height": 5000},
         ),
         (
             "/search_results_summary?query=a",
-            "search_results_summary_mobile.jpg",
+            "search_results_summary_mobile.png",
             {"width": 390, "height": 5000},
         ),
         (
             "/search/transferring_body/8ccc8cd1-c0ee-431d-afad-70cf404ba337?query=a&sort=series-asc&search_filter=test",
-            "search_transferring_body_mobile.jpg",
+            "search_transferring_body_mobile.png",
             {"width": 390, "height": 5000},
         ),
         (
             "/record/123e4567-e89b-12d3-a456-426614174000",
-            "record_mobile.jpg",
+            "record_mobile.png",
             {"width": 390, "height": 5000},
         ),
     ],
@@ -151,7 +151,7 @@ def test_css_no_visual_regression_mobile(
     aau_user_page.set_viewport_size(viewport)
     aau_user_page.goto(url, wait_until="networkidle")
     aau_user_page.wait_for_load_state("domcontentloaded")
-    snapshot = aau_user_page.screenshot(full_page=True, type="jpeg")
+    snapshot = aau_user_page.screenshot(full_page=True, type="png")
     assert_matches_snapshot(
         snapshot, device="mobile", page_name=f"{browser_name}-{screenshot_file}"
     )
