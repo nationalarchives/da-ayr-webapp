3e7cf4562fcff59f3e9223ba3c44f75b7365d7c5
Rahat Ahmed <rahmed@zaizi.com>
2025-08-21 15:43:45 +0100

    inline codeql supression

--- data_management/access_copy_converter/access_copy_converter/main.py
+++ data_management/access_copy_converter/access_copy_converter/main.py
@@ -19,8 +19,8 @@ sm = boto3.client("secretsmanager")
 def get_secret_string(secret_id):
     secret_value = sm.get_secret_value(SecretId=secret_id)
     secret = json.loads(
-        secret_value["SecretString"]
-    )  # pragma: allowlist secret
+        secret_value["SecretString"]  # pragma: allowlist secret
+    )
     return secret
 
 
@@ -136,7 +136,7 @@ def process_consignment(consignment_ref: str):
                     dest_key = f"{consignment_ref}/{file_id}"
                     s3.upload_file(output_path, dest_bucket, dest_key)
                     logger.info(
-                        f"Uploaded converted PDF to s3://{dest_bucket}/{dest_key}"  # @codeql[skip]
+                        f"Uploaded converted PDF to s3://{dest_bucket}/{dest_key}"
                     )
             else:
                 logger.info(f"File {file_id} does not require conversion")
