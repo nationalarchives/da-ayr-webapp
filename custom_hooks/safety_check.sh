cff1cb0e4e197e25383285e81d1d258169deac2c
Colin B <colinbowen2@gmail.com>
2025-09-02 14:15:25 +0100

    safety version

--- custom_hooks/safety_check.sh
+++ custom_hooks/safety_check.sh
@@ -12,5 +12,4 @@ echo "Ensuring compatible safety and typer versions..."
 pip install --force-reinstall typer==0.9.0
 pip install --force-reinstall --no-deps safety==2.3.5
 # The --ignore flag was added here because the vulnerability with ID 70612 as reported by Safety CLI exists for all the latest versions of Jinja, it can be removed once fixed
-poetry export --without-hashes -f requirements.txt
-safety check --full-report --stdin --ignore=70612
+poetry export --without-hashes -f requirements.txt | safety check --full-report --stdin --ignore=70612
