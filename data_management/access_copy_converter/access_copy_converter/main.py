d61e9b73a91753ae9f83786e0fc8a8a758f0f1b4
Rahat Ahmed <rahmed@zaizi.com>
2025-09-12 15:54:54 +0100

    added functions for bulk access copy convertion

--- data_management/access_copy_converter/access_copy_converter/main.py
+++ data_management/access_copy_converter/access_copy_converter/main.py
@@ -5,12 +5,12 @@ import subprocess  # nosec
 import tempfile
 
 import boto3
+from botocore.exceptions import ClientError
 from sqlalchemy import MetaData, Table, create_engine, select
 from sqlalchemy.exc import SQLAlchemyError
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 logger = logging.getLogger()
-
 
 s3 = boto3.client("s3")
 sm = boto3.client("secretsmanager")
@@ -18,10 +18,7 @@ sm = boto3.client("secretsmanager")
 
 def get_secret_string(secret_id):
     secret_value = sm.get_secret_value(SecretId=secret_id)
-    secret = json.loads(
-        secret_value["SecretString"]  # pragma: allowlist secret
-    )
-    return secret
+    return json.loads(secret_value["SecretString"])  # pragma: allowlist secret
 
 
 def get_engine():
@@ -32,6 +29,16 @@ def get_engine():
         f"@{creds['proxy']}:{creds['port']}/{creds['dbname']}"
     )
     return create_engine(url)
+
+
+def already_converted(bucket, key):
+    try:
+        s3.head_object(Bucket=bucket, Key=key)
+        return True
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+        raise
 
 
 def get_extension(file_id, conn, metadata):
@@ -50,7 +57,11 @@ def get_extension(file_id, conn, metadata):
     except SQLAlchemyError as e:
         logger.error(f"Error querying FFIDMetadata table: {e}")
         conn.rollback()
+        return "ERROR"
 
+    logger.info(
+        f"No extension found in FFIDMetadata for {file_id}. Trying to extract from file_name instead"
+    )
     try:
         stmt = (
             select(file_table.c.FileName)
@@ -60,106 +71,168 @@ def get_extension(file_id, conn, metadata):
         result = conn.execute(stmt).first()
         if result and result[0]:
             filename = result[0]
+            logger.info(f"FileName: {filename}")
             if "." in filename:
                 return filename.rsplit(".", 1)[1].lower()
+            logger.warning("No extension in FileName")
     except SQLAlchemyError as e:
         logger.error(f"Error querying File table: {e}")
         conn.rollback()
+        return "ERROR"
 
-    return None
 
-
-def convert_office_to_pdf(input_path, output_path):
+def convert_with_libreoffice(input_path, output_path, convert_to="pdf"):
     try:
         subprocess.run(  # nosec
             [
                 "soffice",
                 "--headless",
                 "--convert-to",
-                "pdf",
+                convert_to,
                 "--outdir",
                 os.path.dirname(output_path),
                 input_path,
             ],
             check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
         )
     except subprocess.CalledProcessError as e:
-        raise RuntimeError(f"LibreOffice conversion failed: {e}")
+        raise RuntimeError(
+            f"LibreOffice conversion failed: {e.stderr.decode()}"
+        )
 
 
-def process_consignment(consignment_ref: str):
-    logger.info(f"Processing consignment: {consignment_ref}")
+def process_consignment(
+    consignment_ref,
+    source_bucket,
+    dest_bucket,
+    convertible_extensions,
+    engine,
+    metadata,
+):
     prefix = f"{consignment_ref}/"
 
-    engine = get_engine()
-    metadata = MetaData()
-    secret_id = os.getenv("APP_SECRET_ID")
-    app_secret = get_secret_string(secret_id)
-    source_bucket = app_secret["RECORD_BUCKET_NAME"]
-    dest_bucket = app_secret["ACCESS_COPY_BUCKET"]
-    convertible_extensions = set(
-        json.loads(app_secret["CONVERTIBLE_EXTENSIONS"])
-    )
-    with engine.connect() as conn:
-        response = s3.list_objects_v2(Bucket=source_bucket, Prefix=prefix)
-        if "Contents" not in response:
-            logger.warning(f"No files found in consignment {consignment_ref}")
-            return
+    response = s3.list_objects_v2(Bucket=source_bucket, Prefix=prefix)
+    if "Contents" not in response:
+        logger.warning(f"No files found in consignment {consignment_ref}")
+        return
 
+    logger.info(
+        f"{len(response['Contents'])} files in consignment {consignment_ref}"
+    )
+
+    with engine.connect() as conn:
         for obj in response["Contents"]:
             key = obj["Key"]
             file_id = key.split("/")[-1]
             logger.info(f"Checking file_id: {file_id}")
 
             extension = get_extension(file_id, conn, metadata)
+            if extension == "ERROR":
+                logger.error(f"Failed to get file_extension for {file_id}")
+                continue
+
             logger.info(f"File extension: {extension}")
 
-            if extension in convertible_extensions:
-                logger.info(f"File {file_id} requires conversion to PDF")
-
-                with tempfile.TemporaryDirectory() as tmpdir:
-                    input_path = os.path.join(tmpdir, f"input.{extension}")
-                    output_path = os.path.join(tmpdir, "input.pdf")
-
-                    s3.download_file(source_bucket, key, input_path)
-                    logger.info(f"Downloaded {key} to {input_path}")
-
-                    try:
-                        convert_office_to_pdf(input_path, output_path)
-                        logger.info(
-                            f"Converted {input_path} to PDF {output_path}"
-                        )
-                    except Exception as e:
-                        logger.error(f"Conversion failed for {file_id}: {e}")
-                        continue
-
-                    dest_key = f"{consignment_ref}/{file_id}"
-                    s3.upload_file(output_path, dest_bucket, dest_key)
-                    logger.info("Uploaded converted PDF to Access Copy Bucket")
-            else:
+            if extension not in convertible_extensions:
                 logger.info(f"File {file_id} does not require conversion")
+                continue
+
+            dest_key = f"{consignment_ref}/{file_id}"
+            if already_converted(dest_bucket, dest_key):
+                logger.info(f"Skipping {file_id}, already converted")
+                continue
+
+            logger.info(f"File {file_id} requires conversion to PDF")
+            with tempfile.TemporaryDirectory() as tmpdir:
+                input_path = os.path.join(tmpdir, f"input.{extension}")
+                output_path = os.path.join(tmpdir, "input.pdf")
+
+                s3.download_file(source_bucket, key, input_path)
+                logger.info(f"Downloaded {key} to {input_path}")
+
+                try:
+                    if extension in ("xls", "xlsx"):
+                        temp_ods = os.path.join(tmpdir, "input.ods")
+                        convert_with_libreoffice(
+                            input_path, temp_ods, convert_to="ods"
+                        )
+                        convert_with_libreoffice(
+                            temp_ods,
+                            output_path,
+                            convert_to='pdf:calc_pdf_Export:{"SinglePageSheets":{"type":"boolean","value":"true"}}',
+                        )
+                    else:
+                        convert_with_libreoffice(input_path, output_path)
+                    logger.info(f"Converted {input_path} to PDF {output_path}")
+                except Exception as e:
+                    logger.error(f"Conversion failed for {file_id}: {e}")
+                    continue
+
+                s3.upload_file(output_path, dest_bucket, dest_key)
+                logger.info(
+                    f"Uploaded converted file {file_id} to Access Copy Bucket"
+                )
 
 
 def main():
-    raw_sns_message = os.getenv("SNS_MESSAGE")
-    logger.info(f"Message Received: {raw_sns_message}")
-    if not raw_sns_message:
-        raise Exception("SNS_MESSAGE environment variable not found")
+    app_secret_id = os.getenv("APP_SECRET_ID")
+    app_secret = get_secret_string(app_secret_id)
+    source_bucket = app_secret["RECORD_BUCKET_NAME"]
+    dest_bucket = app_secret["ACCESS_COPY_BUCKET"]
+    convertible_extensions = set(
+        json.loads(app_secret["CONVERTIBLE_EXTENSIONS"])
+    )
+    engine = get_engine()
+    metadata = MetaData()
+    conversion_type = os.getenv("CONVERSION_TYPE")
+    if conversion_type == "BULK":
+        paginator = s3.get_paginator("list_objects_v2")
+        response = paginator.paginate(Bucket=source_bucket)
+        consignments = set()
+        for page in response:
+            for obj in page["Contents"]:
+                consignments.add(obj["Key"].split("/")[0])
 
-    try:
-        sns_message = json.loads(raw_sns_message)
-    except Exception as e:
-        logger.error(f"Error parsing SNS_MESSAGE: {e}")
-        raise
+        logger.info(f"Found {len(consignments)} consignments")
 
-    consignment_reference = sns_message.get("parameters", {}).get("reference")
+        for consignment_ref in consignments:
+            logger.info(f"Processing consignment: {consignment_ref}")
+            process_consignment(
+                consignment_ref,
+                source_bucket,
+                dest_bucket,
+                convertible_extensions,
+                engine,
+                metadata,
+            )
+    elif conversion_type == "SINGLE":
+        raw_sns_message = os.getenv("SNS_MESSAGE")
+        logger.info(f"Message Received: {raw_sns_message}")
+        if not raw_sns_message:
+            raise Exception("SNS_MESSAGE environment variable not found")
 
-    if not consignment_reference:
-        raise Exception(
-            "Missing reference in SNS Message required for indexing"
+        try:
+            sns_message = json.loads(raw_sns_message)
+        except Exception as e:
+            logger.error(f"Error parsing SNS_MESSAGE: {e}")
+            raise
+
+        consignment_ref = sns_message.get("parameters", {}).get("reference")
+
+        if not consignment_ref:
+            raise Exception("Missing consignment_reference in SNS Message")
+
+        logger.info(f"Processing consignment: {consignment_ref}")
+        process_consignment(
+            consignment_ref,
+            source_bucket,
+            dest_bucket,
+            convertible_extensions,
+            engine,
+            metadata,
         )
-
-    process_consignment(consignment_reference)
 
 
 if __name__ == "__main__":
