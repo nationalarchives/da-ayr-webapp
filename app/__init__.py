71202b3ddde2e60ea3e0ce3d4f1af1854a69275a
Rahat Ahmed <rahmed@zaizi.com>
2026-01-16 16:53:14 +0000

    testing

--- app/__init__.py
+++ app/__init__.py
@@ -10,12 +10,14 @@ from flask_s3 import FlaskS3
 from flask_talisman import Talisman
 from govuk_frontend_wtf.main import WTFormsHelpers
 from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
-from sqlalchemy.pool import QueuePool
 
 from app.logger_config import setup_logging
 from app.main.db.models import db
 from app.main.util.search_utils import OPENSEARCH_FIELD_NAME_MAP
 from configs.aws_secrets_manager_config import AWSSecretsManagerConfig
+
+# from sqlalchemy.pool import QueuePool
+
 
 compress = Compress()
 talisman = Talisman()
@@ -159,6 +161,7 @@ def create_app(config_class, database_uri=None):
     )
     WTFormsHelpers(app)
 
+    # setup database components
     if database_uri:
         app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
         db.init_app(app)
@@ -168,12 +171,12 @@ def create_app(config_class, database_uri=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
         app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
             "creator": get_connection,
-            "poolclass": QueuePool,
-            "pool_size": 5,
-            "max_overflow": 10,
-            "pool_timeout": 30,
-            "pool_recycle": 600,  # recycle idle connections (RDS/NAT safe)
-            "pool_pre_ping": True,
+            # "poolclass": QueuePool,
+            # "pool_size": 5,
+            # "max_overflow": 10,
+            # "pool_timeout": 30,
+            # "pool_recycle": 600,
+            # "pool_pre_ping": True,
         }
         db.init_app(app)
 
@@ -184,11 +187,47 @@ def create_app(config_class, database_uri=None):
 
     @app.context_processor
     def inject_authenticated_view_status():
+        """
+        This context processor is designed to provide information about the authentication
+        status of the current view to the templates.
+        It specifically checks if the `access_token_sign_in_required` attribute is set on
+        the Flask `g` object, indicating whether the current view requires authentication.
+        The `access_token_sign_in_required` attribute is typically set by the
+        `access_token_sign_in_required` decorator applied to specific routes. This decorator
+        is responsible for enforcing access token sign in requirements and protecting routes
+        that require authentication.
+        The primary purpose of this context processor is to make the `authenticated_view`
+        variable available in templates, allowing template logic to conditionally display
+        elements based on whether the current view requires authentication or not.
+        Example Usage in a Template:
+        ```html
+        {% if authenticated_view %}
+            <!-- Display elements for authenticated views -->
+            <div>
+                You are signed in.
+            </div>
+        {% else %}
+            <!-- Display elements for views that do not require authentication -->
+            <div>
+                Please sign in to access this page.
+            </div>
+        {% endif %}
+        ```
+        Note: Ensure that the `access_token_sign_in_required` decorator is appropriately
+        applied to routes where authentication is required for accurate behavior of
+        this context processor.
+        Returns:
+            dict: A dictionary containing the `authenticated_view` variable, which is a
+                boolean indicating whether the current view requires authentication.
+        """
         authenticated_view = getattr(g, "access_token_sign_in_required", False)
         return dict(authenticated_view=authenticated_view)
 
     @app.after_request
     def add_no_caching_headers(r):
+        """
+        Add headers to tell browsers not to cache the pages to protected routes
+        """
         if g.get("access_token_sign_in_required"):
             r.headers["Cache-Control"] = (
                 "public, max-age=0, no-cache, no-store, must-revalidate"
