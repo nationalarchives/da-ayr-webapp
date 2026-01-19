01617d8d1bbc2ee665bd00196717e8724651a418
Rahat Ahmed <rahmed@zaizi.com>
2026-01-19 17:26:58 +0000

    fixed unit tests

--- main_app.py
+++ main_app.py
@@ -8,12 +8,13 @@ if os.getenv("CONFIG_SOURCE") == "AWS_SECRETS_MANAGER":
     from configs.aws_secrets_manager_config import AWSSecretsManagerConfig
 
     config_class = AWSSecretsManagerConfig
+    local_env = False
 elif os.getenv("CONFIG_SOURCE") == "ENVIRONMENT_VARIABLES":
     config_class = EnvConfig
     local_env = True
 else:
     config_class = EnvConfig
-    local_env = False
+    local_env = True
 try:
     app = create_app(config_class, local_env)
 except Exception as e:
