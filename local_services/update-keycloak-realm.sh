a8b7e887c33a93b3534143d5ad05827724cfaa81
karwan-zaizi <kismael@zaizi.com>
2026-01-28 08:18:03 +0000

    fix file permission issue

--- local_services/update-keycloak-realm.sh
+++ local_services/update-keycloak-realm.sh
@@ -6,8 +6,8 @@ WEBAPP_PORT=${WEBAPP_PORT}
 REALM_FILE="/opt/keycloak/data/import/realm-export.json"
 TEMP_FILE="/tmp/realm-export-temp.json"
 
-# Use sed to replace all instances of port 5003 with the configured port
-# Replace both in URLs (:5003/) and in port strings (:5003")
+# Use sed to replace all instances of port 5000 with the configured port
+# Replace both in URLs (:5000/) and in port strings (:5000")
 sed -E 's/:[0-9]+/:'"$WEBAPP_PORT"'/g' \
     "$REALM_FILE" > "$TEMP_FILE"
 
