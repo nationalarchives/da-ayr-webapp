a6700cadfef92e372a30d49eb00539868a91db96
karwan-zaizi <kismael@zaizi.com>
2026-02-17 09:24:51 +0000

    add more tests

--- app/tests/test_process_browse_route.py
+++ app/tests/test_process_browse_route.py
@@ -355,3 +355,138 @@ class TestProcessBrowseRoute:
             assert "page=1" in result.location
         else:
             assert result["current_page"] == 1
+
+    def test_browse_filter_date(
+        self,
+        client: FlaskClient,
+    ):
+        mock_validated_data = {
+            "date_from": "2024-01-01",
+            "date_to": "2025-01-01",
+            "date_from_day": 1,
+            "date_from_month": 1,
+            "date_from_year": 2024,
+            "date_to_day": 1,
+            "date_to_month": 1,
+            "date_to_year": 2025,
+            "transferring_body_filter": "",
+            "series_filter": "",
+            "consignment_reference": "",
+            "file_name": "",
+            "description": "",
+            "date_filter_field": "",
+            "record_status": "all",
+            "sort": "transferring_body",
+            "page": 1,
+            "per_page": 2,
+        }
+        results = process_browse_request(
+            validated_data=mock_validated_data,
+            default_page_size=10,
+            transferring_bodies=["body1", "body2"],
+        )
+
+        assert results["filters"]["date_from"] == "2024-01-01"
+        assert results["filters"]["date_to"] == "2025-01-01"
+        assert results["date_filters"]["from_day"] == "01"
+        assert results["date_filters"]["from_month"] == "01"
+        assert results["date_filters"]["from_year"] == "2024"
+        assert results["date_filters"]["to_day"] == "01"
+        assert results["date_filters"]["to_month"] == "01"
+        assert results["date_filters"]["to_year"] == "2025"
+
+    def test_browse_sorting_order(
+        self,
+        client: FlaskClient,
+    ):
+        mock_validated_data = {
+            "date_from_day": None,
+            "date_from_month": None,
+            "date_from_year": None,
+            "date_to_day": None,
+            "date_to_month": None,
+            "date_to_year": None,
+            "transferring_body_filter": "",
+            "series_filter": "",
+            "consignment_reference": "",
+            "file_name": "",
+            "description": "",
+            "date_filter_field": "",
+            "record_status": "all",
+            "sorting_orders": {"series": "desc"},
+            "page": 1,
+            "per_page": 2,
+        }
+        results = process_browse_request(
+            validated_data=mock_validated_data,
+            default_page_size=10,
+            transferring_bodies=["body1", "body2"],
+        )
+
+        assert results["query_string_parameters"]["sorting_orders"] == {
+            "series": "desc"
+        }
+
+    def test_browse_transferring_body_filter(
+        self,
+        client: FlaskClient,
+    ):
+        mock_validated_data = {
+            "date_from_day": None,
+            "date_from_month": None,
+            "date_from_year": None,
+            "date_to_day": None,
+            "date_to_month": None,
+            "date_to_year": None,
+            "transferring_body_filter": "AYR Test Data Department",
+            "transferring_body": "AYR Test Data Department",
+            "series_filter": "",
+            "consignment_reference": "",
+            "file_name": "",
+            "description": "",
+            "date_filter_field": "",
+            "record_status": "all",
+            "sort": "transferring_body",
+            "page": 1,
+            "per_page": 2,
+        }
+        results = process_browse_request(
+            validated_data=mock_validated_data,
+            default_page_size=10,
+            transferring_bodies=["body1", "body2"],
+        )
+
+        assert (
+            results["filters"]["transferring_body"]
+            == "AYR Test Data Department"
+        )
+
+    def test_browse_series_filter(
+        self,
+        client: FlaskClient,
+    ):
+        mock_validated_data = {
+            "date_from_day": None,
+            "date_from_month": None,
+            "date_from_year": None,
+            "date_to_day": None,
+            "date_to_month": None,
+            "date_to_year": None,
+            "transferring_body_filter": "",
+            "series_filter": "Series 1",
+            "consignment_reference": "",
+            "file_name": "",
+            "description": "",
+            "date_filter_field": "",
+            "record_status": "all",
+            "sort": "transferring_body",
+            "page": 1,
+            "per_page": 2,
+        }
+        results = process_browse_request(
+            validated_data=mock_validated_data,
+            default_page_size=10,
+            transferring_bodies=["body1", "body2"],
+        )
+
+        assert results["filters"]["series"] == "Series 1"
