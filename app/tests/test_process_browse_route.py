b5e17c78fe8e54a7c14c86897038a1263ee43416
Colin B <colinbowen2@gmail.com>
2026-02-18 16:46:58 +0000

    update browse route tests

--- app/tests/test_process_browse_route.py
+++ app/tests/test_process_browse_route.py
@@ -6,13 +6,7 @@ from app.main.process_routes.browse_route import process_browse_request
 
 class TestProcessBrowseRoute:
 
-    @property
-    def route_url(self):
-        return "/browse"
-
-    def test_browse_filter_no_results(
-        self, client: FlaskClient, mock_all_access_user
-    ):
+    def test_browse_filter_no_results(self, client: FlaskClient):
         mock_validated_data = {
             "date_from_day": None,
             "date_from_month": None,
@@ -69,9 +63,7 @@ class TestProcessBrowseRoute:
             "per_page": None,
         }
 
-    def test_browse_display_first_page(
-        self, client: FlaskClient, mock_all_access_user, browse_files
-    ):
+    def test_browse_display_first_page(self, client: FlaskClient, browse_files):
         mock_validated_data = {
             "date_from_day": None,
             "date_from_month": None,
@@ -129,7 +121,7 @@ class TestProcessBrowseRoute:
         }
 
     def test_browse_display_middle_page(
-        self, client: FlaskClient, app, mock_all_access_user, browse_files
+        self, client: FlaskClient, browse_files
     ):
         mock_validated_data = {
             "date_from_day": None,
@@ -187,9 +179,7 @@ class TestProcessBrowseRoute:
             "transferring_body_filter": "",
         }
 
-    def test_browse_display_last_page(
-        self, client: FlaskClient, app, mock_all_access_user, browse_files
-    ):
+    def test_browse_display_last_page(self, client: FlaskClient, browse_files):
         mock_validated_data = {
             "date_from_day": None,
             "date_from_month": None,
@@ -244,82 +234,6 @@ class TestProcessBrowseRoute:
             "sort": "transferring_body",
             "transferring_body_filter": "",
         }
-
-    def test_browse_display_multiple_pages(
-        self, client: FlaskClient, app, mock_all_access_user, browse_files
-    ):
-        mock_validated_data = {
-            "date_from_day": None,
-            "date_from_month": None,
-            "date_from_year": None,
-            "date_to_day": None,
-            "date_to_month": None,
-            "date_to_year": None,
-            "transferring_body_filter": "",
-            "series_filter": "",
-            "consignment_reference": "",
-            "file_name": "",
-            "description": "",
-            "date_filter_field": "",
-            "record_status": "all",
-            "sort": "transferring_body",
-            "page": 1,
-            "per_page": 2,
-        }
-        result = process_browse_request(
-            validated_data=mock_validated_data,
-            default_page_size=10,
-            transferring_bodies=["body1", "body2"],
-        )
-        assert result["current_page"] == 1
-        assert len(result["results"].items) == 2
-        assert result["date_validation_errors"] == {
-            "date_from": [],
-            "date_to": [],
-        }
-        assert result["date_error_fields"] == ""
-        assert result["transferring_bodies"] == ["body1", "body2"]
-        assert result["pagination"] is not None
-        assert result["filters"] == {}
-        assert result["date_filters"] == {}
-        assert result["sorting_orders"] == {"transferring_body": "asc"}
-        assert result["num_records_found"] == 27
-        assert result["query_string_parameters"] == {
-            "consignment_reference": "",
-            "date_filter_field": "",
-            "date_from_day": None,
-            "date_from_month": None,
-            "date_from_year": None,
-            "date_to_day": None,
-            "date_to_month": None,
-            "date_to_year": None,
-            "description": "",
-            "file_name": "",
-            "page": 1,
-            "per_page": 2,
-            "record_status": "all",
-            "series_filter": "",
-            "sort": "transferring_body",
-            "transferring_body_filter": "",
-        }
-
-    def test_browse_submit_search_query(
-        self, client: FlaskClient, mock_all_access_user, browse_files
-    ):
-        """
-        Given an all_access_user accessing the browse page
-        When they make a POST request
-        Then they should see results in content.
-        """
-        mock_all_access_user(client)
-
-        query = "test"
-        response = client.get(f"{self.route_url}", data={"query": query})
-
-        assert response.status_code == 200
-
-        assert b"Search for digital records" in response.data
-        assert b"Browse records 27" in response.data
 
     def test_browse_invalid_page_redirects_to_default(
         self,
@@ -413,7 +327,7 @@ class TestProcessBrowseRoute:
             "description": "",
             "date_filter_field": "",
             "record_status": "all",
-            "sorting_orders": {"series": "desc"},
+            "sort": "series-desc",
             "page": 1,
             "per_page": 2,
         }
@@ -423,9 +337,7 @@ class TestProcessBrowseRoute:
             transferring_bodies=["body1", "body2"],
         )
 
-        assert results["query_string_parameters"]["sorting_orders"] == {
-            "series": "desc"
-        }
+        assert results["sorting_orders"] == {"series": "desc"}
 
     def test_browse_transferring_body_filter(
         self,
@@ -490,3 +402,34 @@ class TestProcessBrowseRoute:
         )
 
         assert results["filters"]["series"] == "Series 1"
+
+    def test_browse_date_validation_error(
+        self,
+        client: FlaskClient,
+    ):
+        mock_validated_data = {
+            "date_from_day": 1,
+            "date_from_month": 1,
+            "date_from_year": 2025,
+            "date_to_day": 1,
+            "date_to_month": 1,
+            "date_to_year": 2024,  # date_to before date_from
+            "transferring_body_filter": "",
+            "series_filter": "",
+            "consignment_reference": "",
+            "file_name": "",
+            "description": "",
+            "date_filter_field": "",
+            "record_status": "all",
+            "sort": "transferring_body",
+            "page": 1,
+            "per_page": 10,
+        }
+        result = process_browse_request(
+            validated_data=mock_validated_data,
+            default_page_size=10,
+            transferring_bodies=["body1", "body2"],
+        )
+
+        assert result["date_validation_errors"]["date_from"] != []
+        assert result["date_error_fields"] != ""
