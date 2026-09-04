a942f5eb7c829f6ebed36ac8820aff7d151830d7
karwan-zaizi <kismael@zaizi.com>
2026-09-04 11:34:04 +0100

    update tests

--- app/tests/test_search.py
+++ app/tests/test_search.py
@@ -264,12 +264,14 @@ class TestSearchResultsSummary:
 
         form_data = {"query": "junk"}
         response = client.get(f"{self.route_url}", data=form_data)
+        page_text = BeautifulSoup(response.data, "html.parser").get_text(
+            " ", strip=True
+        )
 
         assert response.status_code == 200
         assert b"No results found" in response.data
-        assert b"Try changing or removing" in response.data
-        assert b"a search query." in response.data
-        assert b"Try changing or removing one or more filters." in response.data
+        assert "Try changing or removing a search query." in page_text
+        assert "Try changing or removing one or more filters." in page_text
 
     @patch("app.main.util.search_utils.OpenSearch")
     def test_search_results_summary_shows_correct_amount_of_records(
@@ -737,12 +739,14 @@ class TestSearchTransferringBody:
         response = client.get(
             f"{self.route_url}/{transferring_body_id}", data=form_data
         )
+        page_text = BeautifulSoup(response.data, "html.parser").get_text(
+            " ", strip=True
+        )
 
         assert response.status_code == 200
         assert b"No results found" in response.data
-        assert b"Try changing or removing" in response.data
-        assert b"a search query." in response.data
-        assert b"Try changing or removing one or more filters." in response.data
+        assert "Try changing or removing a search query." in page_text
+        assert "Try changing or removing one or more filters." in page_text
 
     @patch("app.main.util.search_utils.OpenSearch")
     def test_search_results_timeout_error(
